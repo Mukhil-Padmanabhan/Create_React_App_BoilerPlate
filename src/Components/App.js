faf1160e5e6961c37310a3df51bc4b7921d33af7
Mukhil Padmanabhan <mpadmanabhan@rxnt.com>
2018-07-24 10:02:13 +0000

    Basic structuring done

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
-import logo from '../Conatiners/logo.svg';
-import '../Conatiners/App.css';
+import logo from '../Containers/logo.svg';
+import '../Containers/App.css';
+import Login from './Login';
+import CreatePatient from './create-patient/createPatient'
+import {HashRouter, Route, Link, Redirect} from 'react-router-dom';
 
-class App extends Component {
+
+class App extends React.Component {
+  constructor(props) {
+		super(props);
+
+	}
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <HashRouter basename="/">
+        <div>
+            <div>
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to My React</h1>
+            </header>
+              <div>
+                {/* <Redirect to={'/'} /> */}
+                <Route exact path="/login" render={(props) => ( <Login/> )} />
+                <Route exact path="/createPatient" render={(props) => ( <CreatePatient/> )} />
+              </div>
+            </div>
+        </div>
+      </HashRouter>
+
     );
   }
 }
---
+++ src/Components/create-patient/createPatient.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class CreatePatient extends React.Component {
+  constructor(props) {
+		super(props);
+
+	}
+render() {
+    return (
+        <div> Hello </div>
+    )
+}
+}
